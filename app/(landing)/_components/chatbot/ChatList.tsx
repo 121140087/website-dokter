c950e30f84e13ebf070029d70a558ebdb1c85c64
121140087 <umy.121140087@student.itera.ac.id>
2025-01-24 08:05:26 +0700

    hide dokter chat when user isnt login

--- app/(landing)/_components/chatbot/ChatList.tsx
+++ app/(landing)/_components/chatbot/ChatList.tsx
@@ -1,7 +1,17 @@
+import { getCurrentUser } from "@/actions/getCurrentUser";
 import { ChatPage } from "@/lib/definitions/enum";
-import { X } from "lucide-react";
+import { User } from "next-auth";
+import { useEffect, useState } from "react";
 
 const ChatList = ({ onChange }: { onChange: (page: ChatPage) => any }) => {
+  const [user, setUser] = useState<User | undefined>();
+  const updateUser = async () => {
+    const session = await getCurrentUser();
+    setUser(session);
+  };
+  useEffect(() => {
+    updateUser();
+  }, []);
   return (
     <div className="flex flex-col h-full ">
       <div
@@ -11,13 +21,15 @@ const ChatList = ({ onChange }: { onChange: (page: ChatPage) => any }) => {
         <div className="rounded-full w-10 h-10 bg-slate-500" />
         <p>Chatbot</p>
       </div>
-      <div
-        className="flex gap-x-4 items-center hover:bg-slate-100 cursor-pointer p-4 "
-        onClick={() => onChange(ChatPage.DOKTER)}
-      >
-        <div className="rounded-full w-10 h-10 bg-slate-500" />
-        <p>Dokter Irawan</p>
-      </div>
+      {user && (
+        <div
+          className="flex gap-x-4 items-center hover:bg-slate-100 cursor-pointer p-4 "
+          onClick={() => onChange(ChatPage.DOKTER)}
+        >
+          <div className="rounded-full w-10 h-10 bg-slate-500" />
+          <p>Dokter Irawan</p>
+        </div>
+      )}
     </div>
   );
 };
