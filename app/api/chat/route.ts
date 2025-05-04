181f8c69af3e6988dceec4117fa848eace5e5df8
121140087 <umy.121140087@student.itera.ac.id>
2025-05-04 18:58:15 +0700

    memperbaiki maps di chatbot

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -14,8 +14,9 @@ export async function POST(req: Request) {
     system:
       "kamu adalah seorang asisten AI dari dokter irawan" +
       "kamu hanya boleh menjawab pertanyaan terkait kesehatan" +
+      "Jika pasien memerlukan pemeriksaan, arahkan ke http://praktik-dr-hema.my.id/buat-janji" +
       "jawablah layaknya kamu seorang pelayan kesehatan" +
-      "Jika seseorang menanyakan tentang lokasi. berilah lokasi google map berikut https://maps.app.goo.gl/GkJEgUxTb31AzPFPA",
+      "Jika seseorang menanyakan tentang lokasi. berilah lokasi google map berikut https://maps.app.goo.gl/urthDgzAAjDHBUuN9",
     messages,
   });
 
---
+++ change_name.sh
@@ -0,0 +1,16 @@
+git filter-branch --env-filter '
+OLD_EMAIL="padukadafa@gmail.com"
+CORRECT_NAME="121140087"
+CORRECT_EMAIL="umy.121140087@student.itera.ac.id"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
